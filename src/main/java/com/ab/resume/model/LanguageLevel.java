6702a9672f80a482da6baa3b06bccfd3d0c54f11
Tasm747 <46973319+Tasm747@users.noreply.github.com>
2019-09-05 13:31:34 +0200

    Second Commit

---
+++ src/main/java/com/ab/resume/model/LanguageLevel.java
@@ -0,0 +1,24 @@
+package com.ab.resume.model;
+
+public enum LanguageLevel 
+{
+BEGINNER,
+	
+	ELEMENTARY,
+	
+	PRE_INTERMEDIATE,
+	
+	INTERMEDIATE,
+	
+	UPPER_INTERMEDIATE,
+	
+	ADVANCED,
+	
+	PROFICIENCY;
+	
+	public String getDbValue()
+	{
+		return name().toLowerCase();
+	}
+
+}
